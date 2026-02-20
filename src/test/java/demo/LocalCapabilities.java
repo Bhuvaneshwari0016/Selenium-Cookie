fcd8961c8f5c91eede1a508a4749dad1d87820c5
Bhuvaneshwari0016 <bhuvana.pcm716@gmail.com>
2026-02-20 17:10:39 +0530

    Update ChromeOptions for headless execution

--- src/test/java/demo/LocalCapabilities.java
+++ src/test/java/demo/LocalCapabilities.java
@@ -11,31 +11,41 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class LocalCapabilities {
-  @Test
-  public void modernOptions() {
-	  ChromeOptions options=new  ChromeOptions();
-	  options.addArguments("--start-maximized");
-	 // options.setAcceptInsecureCerts(true);
+    @Test
+    public void modernOptions() {
+        ChromeOptions options = new ChromeOptions();
+        // Avoid start-maximized in CI; prefer explicit size
+        options.addArguments("--window-size=1920,1080");
 
-	  WebDriver driver=new ChromeDriver(options);
-	  try {
-		  driver.get("https://the-internet.herokuapp.com/");
-		  Capabilities caps=((HasCapabilities) driver).getCapabilities();
-		  System.out.println("Browser: "+caps.getBrowserName());
-		  System.out.println("Browser Version: "+caps.getBrowserVersion());
-		  System.out.println("Platform: "+caps.getPlatformName());
-		  Map<String, Object> chromeOpts = (Map<String, Object>) caps.getCapability("goog:chromeOptions");
-			if (chromeOpts != null) {
-			    System.out.println("Chrome options: " + chromeOpts);
-			    // Example: detect headless
-			    List<String> args = (List<String>) chromeOpts.get("args");
-			    boolean isHeadless = args != null && args.stream().anyMatch(a -> a.contains("headless"));
-			    System.out.println("Headless? " + isHeadless);
-	  }}
-	  finally {
-		  driver.quit();
-	  }
-	  
-  }
-  
+        if ("true".equalsIgnoreCase(System.getenv("HEADLESS"))) {
+            options.addArguments(
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage"
+            );
+        }
+
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://the-internet.herokuapp.com/");
+
+            Capabilities caps = ((HasCapabilities) driver).getCapabilities();
+            System.out.println("Browser: " + caps.getBrowserName());
+            System.out.println("Browser Version: " + caps.getBrowserVersion());
+            System.out.println("Platform: " + caps.getPlatformName());
+
+            @SuppressWarnings("unchecked")
+            Map<String, Object> chromeOpts = (Map<String, Object>) caps.getCapability("goog:chromeOptions");
+            if (chromeOpts != null) {
+                System.out.println("Chrome options: " + chromeOpts);
+
+                @SuppressWarnings("unchecked")
+                List<String> args = (List<String>) chromeOpts.get("args");
+                boolean isHeadless = args != null && args.stream().anyMatch(a -> a.contains("headless"));
+                System.out.println("Headless? " + isHeadless);
+            }
+        } finally {
+            driver.quit();
+        }
+    }
 }
